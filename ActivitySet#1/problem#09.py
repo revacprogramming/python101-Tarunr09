3d87118dc5327674a2863fa8b21aa3ea7e06fff1
AETHER <95666615+Tarunr09@users.noreply.github.com>
2022-05-15 15:35:36 +0000

    9

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,15 @@
 # Lists
 
-filename = "dataset/romeo.txt"
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh :
+    line=line.rstrip()
+    a = line.split()
+    for word in a :
+        if word in lst :
+            continue
+        else :
+            lst.append(word)
+lst.sort()
+print(lst)
