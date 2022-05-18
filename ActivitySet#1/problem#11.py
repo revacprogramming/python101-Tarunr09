c9d2d6046dfcfe5bf0fb9af3337c4d9b66a9c5be
AETHER <95666615+Tarunr09@users.noreply.github.com>
2022-05-18 23:58:02 +0000

    11

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,19 @@
 # Tuples
 
-filename = "dataset/mbox-short.txt"
+fname = input("Enter file name: ")
+fh = open(fname)
+
+d = dict()
+for line in fh :
+    if line.startswith("From ") and not line.startswith("From:"):
+       words = line.split()
+       a =words[5].split(':')
+       b = a[0]
+       d[b] = d.get(b,0) + 1
+
+lst = list()
+for k,v in d.items() :
+    lst.append((k,v))
+lst.sort()
+for k,v in lst :
+    print(k,v)
