0c155d92c10cc1e77a8c98c45d57a661fb6ed3a4
AETHER <95666615+Tarunr09@users.noreply.github.com>
2022-05-21 13:00:04 +0000

    12

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,16 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+
+import re
+hand = open('regex_sum1.txt')
+lst = []
+
+for line in hand:
+    line = line.rstrip()
+    x = re.findall('[0-9]+', line)
+    lst = lst + x
+
+sum=0
+for z in lst:
+    sum = sum + int(z)
+print(sum)
