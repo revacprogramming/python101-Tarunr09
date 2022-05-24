0c13c605088c63c10bb2608d0f3ee071d130c73a
AETHER <95666615+Tarunr09@users.noreply.github.com>
2022-05-24 23:33:47 +0000

    2.a

--- ActivitySet#2/problem#1.py
+++ ActivitySet#2/problem#1.py
@@ -1,12 +1,12 @@
-
-
 def add(a, b):
-    return  # ...
-
+    sum = int(a) + int(b)
+    return sum
 
 def main():
-    a = input  # ...
-    b = input  # ...
+    a = input("Enter the values :")
+    b = input("Enter the values :")
 
     c = add(a, b)
-    print  # ...
+    print(c)
+if __name__=="__main__":
+    main()
