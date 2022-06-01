58065d07c3da389bf3ff4719e802b39ddca56ca3
AETHER <95666615+Tarunr09@users.noreply.github.com>
2022-06-01 13:55:02 +0000

    13

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -7,6 +7,7 @@ mysock.connect(('data.pr4e.org', 80))
 cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
+
 while True:
     data = mysock.recv(512)
     if len(data) < 1:
