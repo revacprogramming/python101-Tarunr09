86af0b5d6ca900c4aa2e11b37c8a7cf8fc2d0485
AETHER <95666615+Tarunr09@users.noreply.github.com>
2022-06-01 13:50:17 +0000

    13

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,8 +1,6 @@
 # Network Programming
 # https://www.py4e.com/lessons/network
-
-
-import socket
+'''import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80))
@@ -15,4 +13,40 @@ while True:
         break
     print(data.decode(),end='')
 
-mysock.close()
+mysock.close()'''
+
+'''
+from urllib import request
+from bs4 import BeautifulSoup
+
+html=request.urlopen('http://py4e-data.dr-chuck.net/comments_42.html').read()
+soup = BeautifulSoup(html)
+tags=soup('span')
+sum=0
+for tag in tags:
+    
+    sum=sum+int(tag.contents[0])
+print(sum)'''
+
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+import re
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+tags = soup('span')
+for tag in tags:
+    x = re.findall('[0-9]+', str(tags))
+
+sum=0
+for i in x:
+    sum = sum + int(i)
+print(sum)
